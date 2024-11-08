cf0bfd145a657f49e97191e97219eda5da5962d2
hanzawnyein <hanzawnyineonline@gmail.com>
2024-11-08 20:31:47 +0630

    add

---
+++ src/control_flow/lesson3.java
@@ -0,0 +1,15 @@
+package control_flow;
+
+public class Lesson3 {
+    public static void main(String args[]){
+        // multiway selection
+        int i=2;
+        if(i>0){
+            System.out.println("i value is positive number.");
+        }else if(i<0){}
+        else {
+            System.out.println("i value is negative number.");
+        }
+    }
+
+}
